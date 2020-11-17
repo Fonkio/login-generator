4b4c9eb3744d1418984404c41dff106543fb37e9
Fonkio <maxime.fabre10@gmail.com>
2020-11-17 11:14:54 +0100

    creation test LoginServiceTest #1

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -8,27 +8,52 @@ import static org.junit.Assert.*;
 
 public class LoginServiceTest {
 
+    LoginService loginService;
+
     @Before
     public void setUp() throws Exception {
+        String [] listStrings = {"abc", "bcd", "aaa"};
+        this.loginService = new LoginService(listStrings);
     }
 
     @After
     public void tearDown() throws Exception {
+        this.loginService = null;
     }
 
     @Test
-    public void loginExists() {
+    public void loginExistsTrue() {
+        assertTrue(this.loginService.loginExists("abc"));
+    }
+
+    @Test
+    public void loginExistsFalse() {
+        assertFalse(this.loginService.loginExists("ab"));
     }
 
     @Test
     public void addLogin() {
+        this.loginService.addLogin("cde");
+        assertTrue(this.loginService.loginExists("cde"));
     }
 
     @Test
-    public void findAllLoginsStartingWith() {
+    public void findAllLoginsStartingWithSize() {
+        assertEquals(2, this.loginService.findAllLoginsStartingWith("a").size());
     }
 
     @Test
-    public void findAllLogins() {
+    public void findAllLoginsStartingWithFirst() {
+        assertEquals("aaa", this.loginService.findAllLoginsStartingWith("a").get(0));
+    }
+
+    @Test
+    public void findAllLoginsSize() {
+        assertEquals(3, this.loginService.findAllLogins().size());
+    }
+
+    @Test
+    public void findAllLoginsFirst() {
+        assertEquals("aaa", this.loginService.findAllLogins().get(0));
     }
 }
